133943f9e1c854b27543f0fd3f74571f91244b01
Resonious <metreckk@gmail.com>
2014-05-13 12:16:57 -0400

    Order index view is all set as far as strictly order information is concerned

--- spec/factories/order_factory.rb
+++ spec/factories/order_factory.rb
@@ -1,19 +1,32 @@
 FactoryGirl.define do
-  factory :empty_order, class: 'Order' do
-    factory :order do
-      name 'Test Order'
-      firstname 'Test'
-      lastname 'Johnson'
-      email 'testj@example.com'
-      twitter '@test'
-      po '123 test'
-      in_hand_by Time.now + 1.day
-      terms "Don't suck"
-      tax_exempt false
-      needs_redo false
-      sales_status :pending
-      delivery_method :ship_to_one
-      phone_number '123-456-7890'
-    end
+  sequence :name do |n|
+    ['Test Order', 'Custom Print Order', 'Custom T-shirt Order',
+      'Crap Order', 'Expencive Order'][n%5]
+  end
+  sequence :firstname do |n|
+    ['Ricky', 'Nigel', 'Chet', 'David', 'Bob', 'Mr.'][n%6]
+  end
+  sequence :lastname do |n|
+    ['Winowiecki', 'Baillie', 'McGillicutty', 'Suckstorff', 'Ross', 'Anderson'][n%6]
+  end
+  sequence :email do |n|
+    ['test@gmail.com', 'coolguy@example.com', 
+     'whatever@umich.edu', 'nice@yahoo.com'].shuffle[n%4]
+  end
+
+  factory :order do
+    name { generate :name }
+    firstname { generate :firstname }
+    lastname { generate :lastname }
+    email { generate :email }
+    twitter '@test'
+    po '123 test'
+    in_hand_by Time.now + 1.day
+    terms "Don't suck"
+    tax_exempt false
+    needs_redo false
+    sales_status :pending
+    delivery_method :ship_to_one
+    phone_number '123-456-7890'
   end
 end
--- spec/views/orders/index.html.erb_spec.rb
+++ spec/views/orders/index.html.erb_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe "orders/index.html.erb" do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'displays all 3 orders' do
+  	assign :orders, [
+  		create(:order, name: 'first'),
+  		create(:order, name: 'second'),
+  		create(:order, name: 'third')
+  	]
+  	render
+  	expect(rendered).to have_selector 'td', text: 'first'
+  	expect(rendered).to have_selector 'td', text: 'second'
+  	expect(rendered).to have_selector 'td', text: 'third'
+	end
+
+	it 'displays Order ID, Order contact info, sales status, and total' do
+		assign :orders, [
+			create(:order, 
+				name: 'o name',
+				firstname: 'o firstname',
+				lastname: 'o lastname',
+				email: 'o@email.com',
+				sales_status: :pending,
+				total: 9.50
+			)
+		]
+		render
+		expect(rendered).to have_selector 'td', text: 'o name'
+		expect(rendered).to have_selector 'td', text: 'o firstname'
+		expect(rendered).to have_selector 'td', text: 'o lastname'
+		expect(rendered).to have_selector 'td', text: 'o@email.com'
+		expect(rendered).to have_selector 'td', text: 'Pending'
+		expect(rendered).to have_selector 'td', text: '$9.50'
+	end
 end
