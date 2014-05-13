7d67f4e2a09fb7e0f91db36f135583f079403314
Resonious <metreckk@gmail.com>
2014-05-13 16:08:12 -0400

    Move partial-specific test code to a separate file

---
+++ spec/views/orders/_table_entry.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe '_table_entry.html.erb' do
+	before :each do
+		render partial: 'orders/table_entry', locals: { order: create(:order,
+    	name: 'o name',
+			firstname: 'o firstname',
+			lastname: 'o lastname',
+			email: 'o@email.com',
+			sales_status: :pending,
+			total: 9.50) }
+	end
+
+	it 'displays Order ID, Order contact info, sales status, and total' do
+		expect(rendered).to have_selector 'td', text: 'o name'
+		expect(rendered).to have_selector 'td', text: 'o firstname'
+		expect(rendered).to have_selector 'td', text: 'o lastname'
+		expect(rendered).to have_selector 'td', text: 'o@email.com'
+		expect(rendered).to have_selector 'td', text: 'Pending'
+		expect(rendered).to have_selector 'td', text: '$9.50'
+	end
+end
--- spec/views/orders/index.html.erb_spec.rb
+++ spec/views/orders/index.html.erb_spec.rb
@@ -12,24 +12,4 @@ describe "orders/index.html.erb" do
   	expect(rendered).to have_selector 'td', text: 'second'
   	expect(rendered).to have_selector 'td', text: 'third'
 	end
-
-	it 'displays Order ID, Order contact info, sales status, and total' do
-		assign :orders, [
-			create(:order, 
-				name: 'o name',
-				firstname: 'o firstname',
-				lastname: 'o lastname',
-				email: 'o@email.com',
-				sales_status: :pending,
-				total: 9.50
-			)
-		]
-		render
-		expect(rendered).to have_selector 'td', text: 'o name'
-		expect(rendered).to have_selector 'td', text: 'o firstname'
-		expect(rendered).to have_selector 'td', text: 'o lastname'
-		expect(rendered).to have_selector 'td', text: 'o@email.com'
-		expect(rendered).to have_selector 'td', text: 'Pending'
-		expect(rendered).to have_selector 'td', text: '$9.50'
-	end
 end
