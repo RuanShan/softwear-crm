6cb8ed4ac5e29dea2bc4a0a820b3a92c28a4aa2b
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-05-15 12:33:11 -0400

    Fixed shipping_method validations

--- app/models/shipping_method.rb
+++ app/models/shipping_method.rb
@@ -1,6 +1,6 @@
 class ShippingMethod < ActiveRecord::Base
-  validates :name, uniqueness: true
-  validates :tracking_url, format: URI::regexp(%w(http https))
+  validates :name, uniqueness: true, presence: true
+  validates :tracking_url, format: {with: URI::regexp(%w(http https)), message: 'should be in format http://www.url.com/path'}
 
   default_scope -> { where(deleted_at: nil) }
   scope :deleted, -> { unscoped.where.not(deleted_at: nil)}
