5a247931e56921f49713b380ac5ee496cd9163f2
Resonious <metreckk@gmail.com>
2014-05-19 12:51:58 -0400

    Clean up some stuff

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -3,11 +3,6 @@ FactoryGirl.define do
 		firstname 'Test'
 		lastname 'User'
 		email 'nobody@annarbortees.com'
-	end
-end
-
-FactoryGirl.modify do
-	factory :user do
 		after(:build) { |u| u.password_confirmation = u.password = 'pw4test' }
 	end
 end
