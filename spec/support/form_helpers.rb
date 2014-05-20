c5b28b3345844429f1697f6cc2819f5d2505f043
Nigel Baillie <metreckk@gmail.com>
2014-05-20 16:02:31 -0500

    Hotfix failure issues in have_field_for
    When testing have_field_for inside a form with a css ID and class both containing the resource name, it would always succeed, even if the expected field was not present. This is fixed

--- spec/support/form_helpers.rb
+++ spec/support/form_helpers.rb
@@ -13,19 +13,22 @@ module FormHelpers
     match do |page|
       doc = Nokogiri::HTML page
       css_pre = (@@model_form_context && @@scope_to_form ? 
-          "form[class*='#{@@model_form_context}'],form[id*='#{@@model_form_context}'] " :
+          "form[class*='#{@@model_form_context}'] " :
           "")
       css_attr = (@@model_form_context ? 
                   "name='#{@@model_form_context}[#{field_name}]'" :
                   "name='#{field_name}'")
 
-      !(doc.css("#{css_pre}input[#{css_attr}]").empty? and
-        doc.css("#{css_pre}select[#{css_attr}]").empty? and
-        doc.css("#{css_pre}textarea[#{css_attr}]").empty? and
-        doc.css("#{css_pre}datetime[#{css_attr}]").empty?)
+      !doc.css("#{css_pre}*[#{css_attr}]").empty?
     end
     failure_message do |page|
-      "Couldn't find field for #{@@model_form_context}[#{field_name}] in page: #{page}"
+      css_pre = (@@model_form_context && @@scope_to_form ? 
+          "form[class*='#{@@model_form_context}'] form[id*='#{@@model_form_context}'] " :
+          "")
+      css_attr = (@@model_form_context ? 
+                  "name='#{@@model_form_context}[#{field_name}]'" :
+                  "name='#{field_name}'")
+      "Couldn't find field for #{css_pre}*#{css_attr} in page: #{page}"
     end
   end
 
