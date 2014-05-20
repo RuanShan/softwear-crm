b4d59a3de226494870eec55f9f7f3cd9a5213953
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-05-19 20:49:59 -0400

    Schema commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,23 @@
 
 ActiveRecord::Schema.define(version: 20140516175307) do
 
+  create_table "activities", force: true do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
+
   create_table "brands", force: true do |t|
     t.string   "name"
     t.string   "sku"
@@ -36,6 +53,30 @@ ActiveRecord::Schema.define(version: 20140516175307) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "deleted_at"
+  end
+
+  create_table "orders", force: true do |t|
+    t.string   "email"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "company"
+    t.string   "twitter"
+    t.string   "name"
+    t.string   "po"
+    t.datetime "in_hand_by"
+    t.string   "terms"
+    t.boolean  "tax_exempt"
+    t.string   "tax_id_number"
+    t.boolean  "is_redo"
+    t.text     "redo_reason"
+    t.string   "sales_status"
+    t.string   "delivery_method"
+    t.decimal  "total",             precision: 10, scale: 2
+    t.datetime "deleted_at"
+    t.string   "phone_number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.decimal  "commission_amount", precision: 10, scale: 2
   end
 
   create_table "shipping_methods", force: true do |t|
