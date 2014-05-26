0e7b5c8226f5ed3bb8469bc58ce3f02fdc34d0cd
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-05-26 00:11:06 -0400

    Using figaro to configure smtp, using smtp for e-mail

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,16 @@ module CrmSoftwearcrmCom
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        address:              ENV['SMTP_ADDRESS'],
+        port:                  ENV['SMTP_PORT'],
+        domain:                ENV['SMTP_DOMAIN'],
+        user_name:             ENV['SMTP_USER_NAME'],
+        password:              ENV['SMTP_PASSWORD'],
+        authentication:        ENV['SMTP_AUTHENTICATION'],
+        enable_starttls_auto: ENV['SMTP_ENABLE_STARTTLS_AUTO']  }
+
 
     config.to_prepare do
       [
