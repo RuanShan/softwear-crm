47e000ddbea2fdf0e97568e89681f60144beb15c
David Suckstorff <david@devminion-CM6630-CM6730-CM6830.(none)>
2014-05-30 15:08:41 -0400

    Add imprintable variant

--- app/models/imprintable_variant.rb
+++ app/models/imprintable_variant.rb
@@ -1,4 +1,9 @@
 class ImprintableVariant < ActiveRecord::Base
+  belongs_to :imprintable
+  belongs_to :size
+  belongs_to :color
+
+  validates_presence_of :imprintable, :size, :color
 
   default_scope -> { where(deleted_at: nil)}
   scope :deleted, -> { unscoped.where.not(deleted_at: nil)}
