62b8a2693eb4ccd305116b94484dd23a9da7e2fc
David Suckstorff <david@devminion-CM6630-CM6730-CM6830.(none)>
2014-06-02 12:03:31 -0400

    Fix Imprintable Variant failures

--- spec/factories/imprintable_variant.rb
+++ spec/factories/imprintable_variant.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :valid_imprintable_variant, class: ImprintableVariant do
     sequence(:imprintable_id) { |n| "imprintable_id#{n}"}
+    size { |s| s.association(:valid_size) }
+    color { |b| b.association(:valid_color) }
+    imprintable { |i| i.association(:valid_imprintable) }
   end
 end
