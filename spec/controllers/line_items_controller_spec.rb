c315edb9f180b3d48368f7c99d79c2a7d00ab391
Resonious <metreckk@gmail.com>
2014-06-06 09:26:24 -0400

    Monstrosity of a spec is all filled in

--- spec/controllers/line_items_controller_spec.rb
+++ spec/controllers/line_items_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe LineItemsController, line_item_spec: true, dear_god: true do
 	render_views
 
-	describe '#options' do
+	describe '#select_options' do
 
 		context 'when there are brands' do
 			2.times { |i| let!("brand#{i}".to_sym) { create(:valid_brand) } }
@@ -34,21 +34,47 @@ describe LineItemsController, line_item_spec: true, dear_god: true do
 				end
 
 				context 'with style_id' do
-					2.times { |i| let!("style#{i}".to_sym) { create(:valid_style, brand_id: brand1.id) } }
+					2.times { |i| let!("color#{i}".to_sym) { create(:valid_color) } }
+					2.times { |i| let!("size#{i}".to_sym) { create(:valid_size) } }
+					let!(:imp) { create(:valid_imprintable, style_id: style1.id) }
+
+					let!(:iv0) { create(:valid_imprintable_variant, imprintable_id: imp.id, color_id: color0.id) }
+					2.times { |i| let!("iv1_#{i}".to_sym) { create(:valid_imprintable_variant, 
+						imprintable_id: imp.id,
+						size_id: send("size#{i}").id,
+						color_id: color1.id
+					) } }
 
 					context 'when there are matching colors' do
-						it 'responds with a select tag for colors'
+						it 'responds with a select tag for colors' do
+							get :options, style_id: style1.id
+							expect(response.body).to include '<select>'
+							expect(response.body).to include color0.name
+							expect(response.body).to include color1.name
+						end
 					end
 					context 'when there are no matching colors' do
-						it 'responds with error message html'
+						it 'responds with error message html' do
+							get :options, style_id: style0.id
+							expect(response.body).to include 'error'
+						end
 					end
 
-					context 'with color_id' do
-						context 'when there is a matching variant' do
-							it 'responds with an imprintable_variant JSON'
+					context 'and color_id' do
+						context 'when there are matching variants' do
+							it 'responds with the name, description, and a list of sizes' do
+								get :options, style_id: style1.id, color_id: color1.id
+								expect(response.body).to include imp.name
+								expect(response.body).to include imp.description
+								expect(response.body).to include size0.name
+								expect(response.body).to include size1.name
+							end
 						end
 						context 'when there is no matching variant' do
-							it 'responds with error message html'
+							it 'responds with error message html' do
+								get :options, style_id: style1.id, color_id: color0.id
+								expect(response.body).to include 'no matching'
+							end
 						end
 					end
 				end
