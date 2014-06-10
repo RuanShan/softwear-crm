6ddc087d5af160f1938de71b15c4c63d3fe99f8b
Resonious <metreckk@gmail.com>
2014-06-10 15:36:44 -0400

    replaced wait_for_ajax with sleep 0.5 in jobs_spec.rb
    Not sure why this helped or what exactly the issue was before, but it was freezing on wait_for_ajax

--- spec/features/jobs_spec.rb
+++ spec/features/jobs_spec.rb
@@ -11,7 +11,7 @@ feature 'Jobs management', js: true, job_spec: true do
 
   scenario 'user visits /orders/1/edit#jobs and is switched to the jobs tab' do
   	visit '/orders/1/edit#jobs'
-  	wait_for_ajax
+  	sleep 0.5
   	expect(page).to have_content 'Test Job'
   end
 
@@ -32,14 +32,14 @@ feature 'Jobs management', js: true, job_spec: true do
   scenario 'user can create a new job, and the form immediately shows up' do
   	visit '/orders/1/edit#jobs'
   	click_button 'New Job'
-  	wait_for_ajax
+  	sleep 0.5
   	expect(all('form[id*="job"]').count).to eq 2
   	expect(Job.all.count).to eq 2
   end
 
   scenario 'creating two jobs in a row does not fail on account of duplicate name' do
   	visit '/orders/1/edit#jobs'
-  	2.times { click_button 'New Job'; wait_for_ajax }
+  	2.times { click_button 'New Job'; sleep 0.5 }
   	expect(all('form[id*="job"').count).to eq 3
   	expect(Job.all.count).to eq 3
   end
@@ -48,12 +48,12 @@ feature 'Jobs management', js: true, job_spec: true do
     order.jobs << create(:job, name: 'Okay Job')
     visit '/orders/1/edit#jobs'
     click_button 'New Job'
-    wait_for_ajax
+    sleep 0.5
 
     within_form_for job do
       fill_in_inline 'name', with: 'Okay Job'
     end
-    wait_for_ajax
+    sleep 0.5
 
     expect(page).to have_content 'Name has already been taken'
   end
@@ -61,16 +61,16 @@ feature 'Jobs management', js: true, job_spec: true do
   scenario 'a job can be deleted' do
     visit '/orders/1/edit#jobs'
     click_button 'Delete Job'
-    wait_for_ajax
+    sleep 0.5
     expect(order.jobs.count).to eq 0
   end
 
   scenario 'a job can be created and deleted without refreshing the page' do
     visit '/orders/1/edit#jobs'
     click_button 'New Job'
-    wait_for_ajax
+    sleep 0.5
     all('button', text: 'Delete Job').last.click
-    wait_for_ajax
+    sleep 0.5
     expect(order.jobs.count).to eq 1
   end
 end
