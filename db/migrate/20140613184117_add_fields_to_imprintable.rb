65ebe34924e67f2d1555a1a9dd23bc390c60f833
David Suckstorff <david@devminion-CM6630-CM6730-CM6830.(none)>
2014-06-16 14:12:40 -0400

    Remove imprintable_linker_table

--- db/migrate/20140613184117_add_fields_to_imprintable.rb
+++ db/migrate/20140613184117_add_fields_to_imprintable.rb
@@ -4,10 +4,4 @@ class AddFieldsToImprintable < ActiveRecord::Migration
     add_column :imprintables, :material, :string
     add_column :imprintables, :standard_offering, :boolean
   end
-
-  create_table :imprintable_linker_table, id: false do |t|
-    t.integer :imprintable_id
-    t.integer :coordinate_id
-    t.integer :store_id
-  end
 end
