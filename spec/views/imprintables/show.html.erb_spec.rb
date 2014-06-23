e9e995cff0108d7aa68dbbab6abfb14fc8800147
David Suckstorff <david@devminion-CM6630-CM6730-CM6830.(none)>
2014-06-23 10:36:05 -0400

    Extend spec to check for standard product notice being rendered
    Remove styling from .html.erb files

--- spec/views/imprintables/show.html.erb_spec.rb
+++ spec/views/imprintables/show.html.erb_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 
 describe 'imprintables/show.html.erb', imprintable_spec: true do
-  let!(:imprintable){ create(:valid_imprintable) }
-  login_user
+  context 'The imprintable is not part of the standard product set' do
+    let!(:imprintable) { create(:valid_imprintable) }
+    login_user
 
-  it 'has a tabbed display with basic info, size/color availability, imprint details, and supplier information listed' do
-    assign(:imprintable, imprintable)
-    render file: 'imprintables/show', id: imprintable.to_param
-    expect(rendered).to have_selector("#basic_info_#{imprintable.id}")
-    expect(rendered).to have_selector("#size_color_availability_#{imprintable.id}")
-    expect(rendered).to have_selector("#imprint_details_#{imprintable.id}")
-    expect(rendered).to have_selector("#supplier_information_#{imprintable.id}")
+    it 'has a tabbed display with basic info, size/color availability, imprint details, and supplier information listed' do
+      assign(:imprintable, imprintable)
+      render file: 'imprintables/show', id: imprintable.to_param
+      expect(rendered).to have_selector("#basic_info_#{imprintable.id}")
+      expect(rendered).to have_selector("#size_color_availability_#{imprintable.id}")
+      expect(rendered).to have_selector("#imprint_details_#{imprintable.id}")
+      expect(rendered).to have_selector("#supplier_information_#{imprintable.id}")
+      expect(rendered).to_not have_content 'This Imprintable is part of the Standard Product Set'
+    end
+  end
+
+  context 'The imprintable is part of the standard product set' do
+    let!(:imprintable) { create(:valid_imprintable, standard_offering: true) }
+    login_user
+
+    it 'displays the standard product notice' do
+      assign(:imprintable, imprintable)
+      render file: 'imprintables/show', id: imprintable.to_param
+      expect(rendered).to have_content 'This Imprintable is part of the Standard Product Set'
+    end
   end
 end
