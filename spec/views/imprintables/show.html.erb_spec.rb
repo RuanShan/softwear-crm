3c0cc2bfbdb646f3e106d934ec7561017387581d
david <david.s@annarbortees.com>
2014-06-24 16:25:47 -0400

    Fix size/color grid displaying every color and size
    Extend test suite

--- spec/views/imprintables/show.html.erb_spec.rb
+++ spec/views/imprintables/show.html.erb_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
-describe 'imprintables/show.html.erb', imprintable_spec: true do
+describe 'imprintables/show.html.erb', imprintable_spec: true, new: true do
+  before(:each) do
+    assign(:size_variants, [])
+    assign(:color_variants, [])
+    assign(:variants_array, [])
+  end
+
   context 'The imprintable is not part of the standard product set' do
     let!(:imprintable) { create(:valid_imprintable) }
     login_user
 
     it 'has a tabbed display with basic info, size/color availability, imprint details, and supplier information listed' do
       assign(:imprintable, imprintable)
+
       render file: 'imprintables/show', id: imprintable.to_param
       expect(rendered).to have_selector("#basic_info_#{imprintable.id}")
       expect(rendered).to have_selector("#size_color_availability_#{imprintable.id}")
@@ -22,7 +29,7 @@ describe 'imprintables/show.html.erb', imprintable_spec: true do
 
     it 'displays the standard product notice' do
       assign(:imprintable, imprintable)
-      render file: 'imprintables/show', id: imprintable.to_param
+      render file: 'imprintables/show', id: imprintable.to_param, locals: { color_variants: [], size_variants: [], variants_array: [] }
       expect(rendered).to have_content 'This Imprintable is part of the Standard Product Set'
     end
   end
