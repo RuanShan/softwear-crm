49df6b72a8aa8d138e9550771973ce8950ff6e32
david <david.s@annarbortees.com>
2014-07-29 12:24:10 -0400

    Minor improvements to quote printing Also comment out mail options section in config/environments/development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,18 +14,18 @@ CrmSoftwearcrmCom::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
 
-  # config.action_mailer.default_url_options = {
-  #   host: '0.0.0.0',
-  #   port: '3000'
-  # }
+  config.action_mailer.default_url_options = {
+    host: '0.0.0.0',
+    port: '3000'
+  }
 
   # using actionMailer, redirect to smtp://localhost:1025
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-  config.action_mailer.default_url_options = { host: 'localhost' }
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # config.action_mailer.default_url_options = { host: 'localhost' }
 
 
       # Print deprecation notices to the Rails logger.
