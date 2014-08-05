90ba0531f63de864b1311636ec54dfa217304c27
Resonious <metreckk@gmail.com>
2014-08-05 12:59:31 -0400

    Beautified jobs_controller.rb

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,45 +1,48 @@
 class JobsController < InheritedResources::Base
+  belongs_to :order, shallow: true
+  respond_to :json
+
   def update
     super do |success, failure|
       success.json { render json: { result: 'success' } }
       failure.json do
         modal_html = 'ERROR'
-        # TODO: Nigel, look at with_format here
-        with_format :html do
-          modal_html = render_to_string(partial: 'shared/modal_errors', locals: { object: @job })
-        end
+        modal_html = render_string(partial: 'shared/modal_errors',
+                                   locals: { object: @job })
 
         render json: {
           result: 'failure',
           errors: @job.errors.messages,
-          modal: modal_html
+          modal:  modal_html
         }
       end
     end
   end
 
   def create
-    # TODO: Nigel, look at this as well
-    @job = Job.create((permitted_params[:job] || {}).merge(order_id: params[:order_id]))
-    # TODO: Nigel, see if using ternary seems gross?
-    if @job.valid?
-      render partial: 'orders/job', locals: { job: @job, animated: true }
-    else
-      render json: {
-        result: 'failure',
-        errors: @job.errors.full_messages
-      }
+    super do |success, failure|
+      success.html do
+        render partial: 'orders/job', locals: { job: @job, animated: true }
+      end
+
+      failure.json do
+        render json: {
+          result: 'failure',
+          errors: @job.errors.full_messages
+        }
+      end
     end
   end
 
   def destroy
-    @job = Job.find params[:id]
-    @job.destroy
-    response = {
-      result: @job.destroyed? ? 'success' : 'failure'
-    }
-    response[:error] = @job.errors.messages[:deletion_status] unless @job.destroyed?
-    render json: response
+    super do |format|
+      format.json do
+        render json: {
+          result: @job.destroyed? ? 'success' : 'failure',
+          error: @job.destroyed? ? @job.errors.messages[:deletion_status] : nil
+        }
+      end
+    end
   end
 
   def show
@@ -59,8 +62,11 @@ class JobsController < InheritedResources::Base
   private
 
   def permitted_params
-    params.permit(:order_id, :job_id, :ids, job: [
-      :id, :name, :description, :collapsed
-    ], imprint: [:print_location_id])
+    params.permit(
+        :order_id, :job_id, :ids,
+
+        job:     [:id, :name, :description, :collapsed], 
+        imprint: [:print_location_id]
+    )
   end
 end
