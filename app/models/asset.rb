340d8d3b143a14d1d70d24220e6791aaf8c3fd51
Nicholas Catoni <nicholas_catoni@brown.edu>
2014-08-06 10:48:31 -0400

    Refactored/styled Assets:
    For Model, moved url and path into variable definitions to remove long ternary

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,15 +1,27 @@
 class Asset < ActiveRecord::Base
   acts_as_paranoid
 
+  path = if Paperclip::Attachment.default_options[:storage] == :s3
+           '/public/assets/:assetable_type/:assetable_id/asset/:style/:id.:extension'
+         else
+           "#{Rails.root}/public/assets/:assetable_type/:assetable_id/asset/:style/:id.:extension"
+         end
+
+  url = if Paperclip::Attachment.default_options[:storage] == :s3
+          '/public/assets/:assetable_type/:assetable_id/asset/:style/:id.:extension'
+        else
+          '/assets/:assetable_type/:assetable_id/asset/:style/:id.:extension'
+        end
+
   belongs_to :assetable, polymorphic: true
 
   validates :description, presence: true
-  # TODO: line length?
+
   do_not_validate_attachment_file_type :file
   has_attached_file :file,
-                    url: Paperclip::Attachment.default_options[:storage] == :s3 ? '/public/assets/:assetable_type/:assetable_id/asset/:style/:id.:extension' : '/assets/:assetable_type/:assetable_id/asset/:style/:id.:extension',
-                    path: Paperclip::Attachment.default_options[:storage] == :s3 ? '/public/assets/:assetable_type/:assetable_id/asset/:style/:id.:extension' : "#{Rails.root}/public/assets/:assetable_type/:assetable_id/asset/:style/:id.:extension",
-                    styles: { :thumb => ['100x100#'], :medium => ['250x250#'] }
+                    path: path,
+                    url: url,
+                    styles: { thumb: ['100x100#'], medium: ['250x250#'] }
   validates_attachment_presence :file
   validates_attachment_size :file, less_than: 120.megabytes
 end
