63c292f6e0aa9fa86cfc0438b5bd79d5c0f4b084
Nicholas Catoni <nicholas_catoni@brown.edu>
2014-08-11 11:02:04 -0400

    Refactored/styled Model Specs for Artwork Request, Artwork, Asset, Imprint Method, Ink Color, Print Location, and Proof:
    Removed should for is_expected.to
    
    General formatting
    
    Added tests for paranoid

--- spec/models/print_location_spec.rb
+++ spec/models/print_location_spec.rb
@@ -2,19 +2,20 @@ require 'spec_helper'
 
 describe PrintLocation do
 
-  describe 'Validations' do
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:max_height) }
-    it { should validate_presence_of(:max_width) }
-    it { should validate_numericality_of (:max_height) }
-    it { should validate_numericality_of(:max_width) }
-    #TODO Nigel thinks the rspec shoulda matchers just aren't working with his uniqueness/scoped/deleted_at patch, "Shoulda matchers is busted"
-    # it { should validate_uniqueness_of(:name).scoped_to(:imprint_method) }
-  end
+  it { is_expected.to be_paranoid }
 
   describe 'Relationships' do
-    it { should belong_to(:imprint_method) }
-    it { should have_many :imprints }
+    it { is_expected.to belong_to(:imprint_method) }
+    it { is_expected.to have_many :imprints }
   end
 
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of(:max_height) }
+    it { is_expected.to validate_presence_of(:max_width) }
+    it { is_expected.to validate_numericality_of (:max_height) }
+    it { is_expected.to validate_numericality_of(:max_width) }
+    it { is_expected.to validate_presence_of(:name) }
+    #TODO Nigel thinks the rspec shoulda matchers just aren't working with his uniqueness/scoped/deleted_at patch, "Shoulda matchers is busted"
+    # it { is_expected.to validate_uniqueness_of(:name).scoped_to(:imprint_method) }
+  end
 end
