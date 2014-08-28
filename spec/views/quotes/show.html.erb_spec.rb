bbeca1e0fb9473b77af561fa3e3db393258cb9df
David Suckstorff <david.s@annarbortees.com>
2014-08-28 09:08:14 -0400

    Quote now displays full name, not first and last

--- spec/views/quotes/show.html.erb_spec.rb
+++ spec/views/quotes/show.html.erb_spec.rb
@@ -12,15 +12,13 @@ describe 'quotes/show.html.erb', quote_spec: true do
     expect(rendered).to have_css('h2', text: 'Customer Information')
     expect(rendered).to have_css('label', text: 'Email:')
     expect(rendered).to have_css('label', text: 'Phone Number:')
-    expect(rendered).to have_css('label', text: 'First Name:')
-    expect(rendered).to have_css('label', text: 'Last Name:')
+    expect(rendered).to have_css('label', text: 'Name:')
     expect(rendered).to have_css('label', text: 'Company:')
     expect(rendered).to have_css('label', text: 'Twitter:')
 
     expect(rendered).to have_content(quote.email)
     expect(rendered).to have_content(quote.phone_number)
-    expect(rendered).to have_content(quote.first_name)
-    expect(rendered).to have_content(quote.last_name)
+    expect(rendered).to have_content(quote.full_name)
     expect(rendered).to have_content(quote.company)
     expect(rendered).to have_content(quote.twitter)
   end
