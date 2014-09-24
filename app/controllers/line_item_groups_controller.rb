e9a1aff4fea163961a78504d39e8fb0f81d9699b
Resonious <metreckk@gmail.com>
2014-09-24 14:38:45 -0400

    Oh look it works now

--- app/controllers/line_item_groups_controller.rb
+++ app/controllers/line_item_groups_controller.rb
@@ -2,11 +2,14 @@ class LineItemGroupsController < InheritedResources::Base
   belongs_to :quote, shallow: true
   respond_to :js
 
-  # def update
-  #   super do |format|
-  #     format.js
-  #   end
-  # end
+  def update
+    super do |format|
+      format.js
+      format.json do
+        render json: { result: @line_item_group.valid? ? 'success' : 'failure' }
+      end
+    end
+  end
 
   # def create
   #   super do |format|
@@ -18,7 +21,7 @@ class LineItemGroupsController < InheritedResources::Base
 
   def permitted_params
     params.permit(
-      :quote_id, :id,
+      :quote_id, :id, :authenticity_token,
 
       line_item_group: [:name, :description]
     )
