312e724a3bd553b99cfebb1483f7479465a05a4b
David Suckstorff <david.s@annarbortees.com>
2014-10-15 14:00:58 -0400

    Add a few more specs that I forgot about

--- spec/models/name_number_spec.rb
+++ spec/models/name_number_spec.rb
@@ -7,4 +7,11 @@ describe NameNumber, name_number_spec: true do
       it { is_expected.to belong_to :imprintable_variant }
     end
   end
+
+  describe 'Validations' do
+    context 'when testing story_190', story_190: true do
+      it { is_expected.to validate_presence_of :imprint_id }
+      it { is_expected.to validate_presence_of :imprintable_variant_id }
+    end
+  end
 end
