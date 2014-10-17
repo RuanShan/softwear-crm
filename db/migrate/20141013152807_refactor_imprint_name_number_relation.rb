e9c992965eda345239bb24f0ed9e7063b1692fbc
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2014-10-16 20:00:52 -0400

    Fixed sunspot yml config, fixed name_number relation garb

--- db/migrate/20141013152807_refactor_imprint_name_number_relation.rb
+++ db/migrate/20141013152807_refactor_imprint_name_number_relation.rb
@@ -4,7 +4,6 @@ class RefactorImprintNameNumberRelation < ActiveRecord::Migration
     add_column :name_numbers, :imprintable_variant_id, :integer
     remove_column :name_numbers, :description, :string
 
-    add_column :imprints, :name_format, :string
     add_column :imprints, :number_format, :string
   end
 end
