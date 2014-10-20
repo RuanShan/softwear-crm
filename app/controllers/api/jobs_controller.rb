b3e5e44620be1da8da92fc82daea949ff15b4405
Resonious <metreckk@gmail.com>
2014-10-20 10:12:02 -0400

    (198) Done

---
+++ app/controllers/api/jobs_controller.rb
@@ -0,0 +1,4 @@
+module Api
+  class JobsController < ApiController
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -104,6 +104,7 @@ CrmSoftwearcrmCom::Application.routes.draw do
 
   namespace 'api' do
     resources 'orders', only: [:index, :show]
+    resources 'jobs', only: [:index, :show]
     resources 'imprintables'
     resources 'colors'
     resources 'sizes'
