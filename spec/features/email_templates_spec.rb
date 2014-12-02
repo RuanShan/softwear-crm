0a2121da738d80cad6872c9945b1eb64dd0df58c
David Suckstorff <david.s@annarbortees.com>
2014-12-02 13:25:08 -0500

    Email features spec

--- spec/features/email_templates_spec.rb
+++ spec/features/email_templates_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 include ApplicationHelper
 
-feature 'Email Templates Management', js: true, email_template_spec: true, story_265: true do
+feature 'Email Templates Management', js: true, email_template_spec: true, story_265: true, new: true do
   given!(:email_template) { create(:valid_email_template) }
   given!(:valid_user) { create(:alternate_user) }
   background(:each) { login_as valid_user }
@@ -12,32 +12,35 @@ feature 'Email Templates Management', js: true, email_template_spec: true, story
     click_link 'Configuration'
     click_link 'Email Templates'
     expect(page).to have_css('tbody')
+    expect(page).to have_content('View and edit email templates')
   end
 
   scenario 'A user can create an email template' do
     visit email_templates_path
-    click_link 'New Email Template'
-    fill_in 'Template', with: 'New template!!'
-    click_button 'Create Template'
-    expect(EmailTemplate.where(template: 'New template!!')).to exist
+    click_link 'Add an Email Template'
+    fill_in 'Subject', with: 'Your new quote from Ann Arbor Tees'
+    fill_in 'Body', with: 'Thank you {{quote.name}} for your purchase'
+    click_button 'Create Email Template'
+    expect(EmailTemplate.where(subject: 'Your new quote from Ann Arbor Tees')).to exist
   end
 
   scenario 'A user can edit an email template' do
     visit email_templates_path
     find("a[href='/configuration/email_templates/#{email_template.id}/edit']").click
-    fill_in 'Template', with: 'New template lololol'
-    click_button 'Update Template'
-    expect(EmailTemplate.where(template: 'New template lololol')).to exist
+    fill_in 'Subject', with: 'New template lololol'
+    click_button 'Update Email Template'
+    expect(EmailTemplate.where(subject: 'New template lololol')).to exist
   end
 
   scenario 'A user can delete an email template' do
     visit email_templates_path
-    find("a[href='configuration/email_templates/#{email_template.id}']").click
+    find("a[href='/configuration/email_templates/#{email_template.id}']").click
     page.driver.browser.switch_to.alert.accept
-    expect(EmailTemplate.where(template: email_template.template)).to_not exist
+    wait_for_ajax
+    expect(EmailTemplate.where(subject: email_template.subject).blank?).to be_truthy
   end
 
-  scenario 'A user can email a template to a customer' do
-  #   TODO: fill this mother in
+  scenario 'A user can email a template to a customer', pending: 'Fill this mother in' do
+    expect(false).to be_truthy
   end
 end
