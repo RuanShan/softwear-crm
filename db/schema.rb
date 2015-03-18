8a8d518b22a4a2f8d516ab3666ed8481ed92bfa3
Resonious <metreckk@gmail.com>
2015-03-18 15:23:13 -0400

    (472) horribly hacky, but works :/

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150318154124) do
+ActiveRecord::Schema.define(version: 20150318165419) do
 
   create_table "activities", force: true do |t|
     t.integer  "trackable_id"
@@ -419,6 +419,7 @@ ActiveRecord::Schema.define(version: 20150318154124) do
     t.datetime "updated_at"
     t.integer  "salesperson_id"
     t.string   "status"
+    t.string   "reason"
   end
 
   create_table "quotes", force: true do |t|
