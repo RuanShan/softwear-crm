0c621ea4da6365fce1b0fb5f03c3aa45059bb1bb
Nigel Baillie <metreckk@gmail.com>
2015-05-01 16:10:06 -0400

    (518) appease ricky (and fix templates)

--- app/assets/javascripts/emails.js
+++ app/assets/javascripts/emails.js
@@ -1,36 +1,49 @@
-$(function() {
-  $('#freshdesk-copypaste-btn').click(function(event) {
-    event.preventDefault();
+function freshdeskCopyBtn(event) {
+  event.preventDefault();
 
-    var quoteId = $(this).data('quote-id');
-    var serialized = $('form.new_email').serializeArray();
-    var data = {email: {}};
+  var quoteId = $(this).data('quote-id');
+  var serialized = $('form.new_email').serializeArray();
+  var data = {email: {}};
 
-    serialized.forEach(function(item) {
-      if (item.name == 'quote_id') {
-        data.quote_id = item.value;
-        return;
-      }
+  serialized.forEach(function(item) {
+    if (item.name == 'quote_id') {
+      data.quote_id = item.value;
+      return;
+    }
 
-      var name = item.name.match(/\[\w+\]/g);
-      if (!name) return;
-      name = name[0];
-      var brackets = /[\[\]]/g;
-      if (!name.match(brackets)) return;
-      name = name.replace(brackets, '');
+    var name = item.name.match(/\[\w+\]/g);
+    if (!name) return;
+    name = name[0];
+    var brackets = /[\[\]]/g;
+    if (!name.match(brackets)) return;
+    name = name.replace(brackets, '');
 
-      data.email[name] = item.value;
-    });
-    data.email.body = $('#email_body').code();
-
-    $.ajax({
-      method: 'POST',
-      url: Routes.freshdesk_quote_emails_path(quoteId),
-
-      data: data,
-      dataType: 'script'
-    });
+    data.email[name] = item.value;
   });
+  data.email.body = $('#email_body').code();
+
+  $.ajax({
+    method: 'POST',
+    url: Routes.freshdesk_quote_emails_path(quoteId),
+
+    data: data,
+    dataType: 'script'
+  });
+}
+
+$(function() {
+  $('#email-template-select').change(function() {
+    if ($(this).val() != '') {
+      $.ajax({
+        method: 'GET',
+        url: "/" + $(this).attr('data-model') + "s/" + $(this).attr('data-record-id') + '/emails/new',
+        data: { email_template_id: $(this).val() },
+        dataType: 'script'
+      });
+    }
+  });
+
+  $('#freshdesk-copypaste-btn').click(freshdeskCopyBtn);
 });
 
 function SelectText(element) {
