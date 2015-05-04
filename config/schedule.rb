e81a2b3036931c80d4824f0d634158c40842e9c1
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-05-04 13:12:20 -0400

    Staggering rake tasks

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,14 +2,14 @@
 set :output, {:error => '/home/ubuntu/RailsApps/crm.softwearcrm.com/shared/log/cron.error.log',
               :standard => '/home/ubuntu/RailsApps/crm.softwearcrm.com/shared/log/cron.log'}
 
-every 30.minutes do
+every :hour, at: 0 do
   rake "data:backup"
+end
+
+every :hour, at: 30 do
+  rake "quote_requests:notify_sales_of_bad_quotes"
 end
 
 every 5.minutes do
   rake "quote_requests:import_from_wordpress"
-end
-
-every 60.minutes do
-  rake "quote_requests:notify_sales_of_bad_quotes"
 end
