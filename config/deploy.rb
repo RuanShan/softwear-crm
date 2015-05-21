720755ed2afa8c8ae3af4eb1aa334f204dbc4960
Nigel Baillie <metreckk@gmail.com>
2015-05-21 10:52:36 -0400

    try rvm_bin_path deploy env

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,6 +33,7 @@ set :linked_files, %w{config/database.yml config/application.yml
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { rvm_bin_path: '~/.rvm/bin' }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
