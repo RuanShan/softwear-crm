3aebb4dda387cfeee904cf5fdb84d64ae957c725
Ben Cooper <ben@wolverinetshirtcompany.com>
2015-05-26 16:52:23 -0400

    (601) Finished

--- spec/features/freshdesk_spec.rb
+++ spec/features/freshdesk_spec.rb
@@ -9,20 +9,32 @@ feature 'FreshdeskTicketViewing' do
 
   context 'a quote has a freshdesk ticket, and that ticket has quoted text' do 
    given!(:quote) { create(:valid_quote) }
+   given!(:fd_ticket) do   {
+    "created_at" => "Thu, 21 May 2015 15:57:32 EDT -04:00",  
+    "notes" => 
+    [
+      {"note" => {"body_html" => "<p></p><blockquote class=\"freshdesk_quote\">wasabi451</blockquote>", "created_at" => "aba" }} 
+    ]
+   }
+   end
    
     background(:each) do 
-      allow(quote).to receive(:no_ticket_id_entered?).and_return false
-      allow(quote).to receive(:no_fd_login?).and_return false
-      allow(quote).to receive(:has_freshdesk_ticket?).and_return true
-    end
+      allow_any_instance_of(Quote).to receive(:no_ticket_id_entered?).and_return false
+      allow_any_instance_of(Quote).to receive(:no_fd_login?).and_return false
+      allow_any_instance_of(Quote).to receive(:has_freshdesk_ticket?).and_return true
+      allow_any_instance_of(Quote).to receive(:get_freshdesk_ticket).and_return fd_ticket
+      allow(fd_ticket).to receive(:helpdesk_ticket).and_return fd_ticket
+      allow_any_instance_of(ApplicationHelper).to receive(:parse_freshdesk_time).and_return Time.now
 
+    end
       
     scenario 'A user can toggle quoted freshdesk text', js: true do
       visit edit_quote_path(quote)
-      click_link 'Details'
-      # make sure quoted text is hidden
+      expect(page).not_to have_content "wasabi451"
       click_link 'Toggle Quoted Text'
-     # make sure quoted text is shown
+      expect(page).to have_content "wasabi451"
+      click_link 'Toggle Quoted Text'
+      expect(page).not_to have_content "wasabi451"
     end
   end
 
