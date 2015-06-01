cc0eb7bf3a912d93f2354877bce206b02ff6f65e
Nigel Baillie <metreckk@gmail.com>
2015-06-01 13:27:18 -0400

    (636)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,18 @@
 require 'sidekiq'
 require 'sidekiq-status'
 
+redis_url = "#{Figaro.env.redis_path}/#{Figaro.env.redis_db || 0}"
+
 Sidekiq.configure_client do |config|
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware
   end
-  config.redis = { :namespace => 'softwear-crm', :url => 'redis://127.0.0.1:6379/0' }
+  config.redis = { :namespace => 'softwear-crm', :url => redis_url }
 end
 
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
   end
-  config.redis = { :namespace => 'softwear-crm', :url => 'redis://127.0.0.1:6379/0' }
+  config.redis = { :namespace => 'softwear-crm', redis_url }
 end
