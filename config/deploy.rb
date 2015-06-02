ec52ac69eb29653cd0fcde9eb8bd3c069fdfee9a
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-06-02 13:18:15 -0400

    Trying to fix restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,11 +43,12 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
+    on roles(:web), in: :sequence, wait: 5 do
+      execute :mkdir, '-p', "#{ release_path }/tmp"
       execute :touch, release_path.join('tmp/restart.txt')
     end
   end
+
 
   after :publishing, :restart
 
