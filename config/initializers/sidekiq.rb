9d03fc0ee8cf5076f3874db08fba304a92c2c663
Nigel Baillie <metreckk@gmail.com>
2015-06-02 15:52:25 -0400

    (587) made api controller CORS-compliant

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,12 +7,12 @@ Sidekiq.configure_client do |config|
   config.client_middleware do |chain|
     chain.add Sidekiq::Status::ClientMiddleware
   end
-  config.redis = { :namespace => 'softwear-crm', :url => redis_url }
+  config.redis = { namespace: 'softwear-crm', url: redis_url }
 end
 
 Sidekiq.configure_server do |config|
   config.server_middleware do |chain|
     chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
   end
-  config.redis = { :namespace => 'softwear-crm', redis_url }
+  config.redis = { namespace: 'softwear-crm', url: redis_url }
 end
