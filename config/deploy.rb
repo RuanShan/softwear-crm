661cfa49e1a92c22c8bc25fdadfb0513a46dd0e0
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-06-02 16:22:49 -0400

    Deployment fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -43,7 +43,7 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:web), in: :sequence, wait: 5 do
+    on roles([:web, :app]), in: :sequence, wait: 5 do
       execute :mkdir, '-p', "#{ release_path }/tmp"
       execute :touch, release_path.join('tmp/restart.txt')
     end
