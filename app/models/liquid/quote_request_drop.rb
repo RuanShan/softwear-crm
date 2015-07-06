cf97e5bdbbf1cff1b9fb4c7c44e7ed1d466b8843
Nigel Baillie <metreckk@gmail.com>
2015-07-06 14:28:07 -0400

    make quote request drop load correctly even with no database

--- app/models/liquid/quote_request_drop.rb
+++ app/models/liquid/quote_request_drop.rb
@@ -3,12 +3,16 @@ class QuoteRequestDrop < Liquid::Drop
     @quote_request = qr
   end
 
-  QuoteRequest.column_names.each do |field|
-    unless /_id$/ =~ field
-      define_method field do
-        @quote_request.send(field)
+  begin
+    QuoteRequest.column_names.each do |field|
+      unless /_id$/ =~ field
+        define_method field do
+          @quote_request.send(field)
+        end
       end
     end
+  rescue ActiveRecord::StatementInvalid => _e
+    logger.warn "Either something's wrong with Quote Requests, or you're running migrations right now."
   end
 
   def salesperson_first_name
