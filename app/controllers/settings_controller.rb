91e4df31bbcf70215886fca3fdc93e2388b7d49c
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-08-18 21:49:48 -0400

    (860) production integration, pending API update and need to rework API

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -10,11 +10,17 @@ class SettingsController < InheritedResources::Base
     @insightly_settings = {
       api_key: Setting.find_or_create_by(name: 'insightly_api_key')
     }
+    @production_crm_settings = {
+     endpoint: Setting.find_or_create_by(name: 'softwear_production_endpoint'),
+     email: Setting.find_or_create_by(name: 'softwear_production_email'),
+     token: Setting.find_or_create_by(name: 'softwear_production_token')
+    }
   end
 
   def update
     Setting.update(params[:fd_settings].keys, params[:fd_settings].values)
     Setting.update(params[:in_settings].keys, params[:in_settings].values)
+    Setting.update(params[:production_crm_settings].keys, params[:production_crm_settings].values)
     redirect_to integrated_crms_path
   end
 
