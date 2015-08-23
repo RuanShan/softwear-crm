fbb331b443d8278b480c0ea6be71ee4211e9ca76
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2015-08-23 14:30:32 -0400

    Fixed specs

--- spec/views/artwork_requests/_artwork_imprint_method_fields.html.erb_spec.rb
+++ spec/views/artwork_requests/_artwork_imprint_method_fields.html.erb_spec.rb
@@ -7,6 +7,6 @@ describe 'artwork_requests/_artwork_imprint_method_fields.html.erb', artwork_req
   before(:each) { render partial: 'artwork_imprint_method_fields', locals: { artwork_request: nil, ink_colors: [ink_color] } }
 
   it 'displays the correct form fields for print locations and ink colors' do
-    expect(rendered).to have_selector('input#artwork_request_ink_color_ids_')
+    expect(rendered).to have_selector("select#artwork_request_ink_color_ids_")
   end
 end
