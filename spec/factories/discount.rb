72000ad3c538c237494e3d992987055ba835dbcd
Nigel Baillie <metreckk@gmail.com>
2015-09-11 14:22:57 -0400

    fix busted discount spec

--- spec/factories/discount.rb
+++ spec/factories/discount.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :discount do
     discount_method 'Cash'
     reason 'whatever'
+    amount 0
 
     factory :discount_with_order do
       discountable { |x| x.association(:order_with_job) }
