b007d39c98ab6fa3f7a951c093687d068853246b
Nigel Baillie <metreckk@gmail.com>
2015-10-23 15:44:59 -0400

    update schema woops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151022201910) do
+ActiveRecord::Schema.define(version: 20151023170916) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -360,6 +360,7 @@ ActiveRecord::Schema.define(version: 20151022201910) do
     t.decimal  "package_width",                        precision: 10
     t.decimal  "package_length",                       precision: 10
     t.string   "tag",                    limit: 255,                            default: "Not Specified"
+    t.string   "marketplace_name",       limit: 255
   end
 
   add_index "imprintables", ["deleted_at"], name: "index_imprintables_on_deleted_at", using: :btree
@@ -615,8 +616,8 @@ ActiveRecord::Schema.define(version: 20151022201910) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.decimal  "shipping",                                     precision: 10, scale: 2
-    t.datetime "initialized_at"
     t.string   "quote_source",                     limit: 255
+    t.datetime "initialized_at"
     t.string   "freshdesk_ticket_id",              limit: 255
     t.boolean  "informal"
     t.integer  "insightly_category_id",            limit: 4
