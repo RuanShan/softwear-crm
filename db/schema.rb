5994ddd1c7cf2e2f23d44b7941a0ebd9c40472ec
Nigel Baillie <metreckk@gmail.com>
2015-11-19 15:44:39 -0500

    migrate for new field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151106161917) do
+ActiveRecord::Schema.define(version: 20151119201921) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "trackable_id",   limit: 4
@@ -762,6 +762,7 @@ ActiveRecord::Schema.define(version: 20151106161917) do
     t.datetime "created_at",                                                null: false
     t.datetime "updated_at",                                                null: false
     t.text     "notes",              limit: 65535
+    t.decimal  "time_in_transit",                  precision: 10, scale: 2
   end
 
   create_table "shipping_methods", force: :cascade do |t|
