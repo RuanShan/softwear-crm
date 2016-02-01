ab69eb01a90b9f06b1f3acfb589e9dbb9986b7db
Nigel Baillie <metreckk@gmail.com>
2016-02-01 14:49:36 -0500

    fba spreadsheet upload now supports job templates

--- app/models/fba_spreadsheet_upload.rb
+++ app/models/fba_spreadsheet_upload.rb
@@ -6,6 +6,7 @@ class FbaSpreadsheetUpload < ActiveRecord::Base
   def spreadsheet=(value)
     @spreadsheet_hash = nil
     if value.respond_to?(:path)
+      self.filename = File.basename(value.try(:original_filename) || value.path)
       super(SimpleXlsxReader.open(value.path).to_hash.to_json)
     else
       super
@@ -29,7 +30,11 @@ class FbaSpreadsheetUpload < ActiveRecord::Base
     all_errors = {}
 
     spreadsheet.each do |sheet_name, rows|
-      next if /IGNORE/ =~ sheet_name
+      if /IGNORE/ =~ sheet_name
+        # Not really an error but this lets them know that it wasn't touched
+        all_errors[sheet_name] = { 1 => %(Name contains "IGNORE" - skipped) }
+        next
+      end
 
       header = {}
       errors = {}
@@ -90,7 +95,10 @@ class FbaSpreadsheetUpload < ActiveRecord::Base
           .where(sizes: { display_value: size_display_value })
           .first
 
-        next "No matching imprintable variant found" if imprintable_variant.nil?
+        if imprintable_variant.nil?
+          next "No matching imprintable variant found: " \
+            "#{brand_name} #{style_catalog_no} - #{color_name}, #{size_display_value}"
+        end
 
         fba_job_template = FbaJobTemplate.find_by name: fba_job_template_name
 
@@ -120,7 +128,7 @@ class FbaSpreadsheetUpload < ActiveRecord::Base
         true
       end
 
-      read = lambda do |row, rownum|
+      read_fba_product = lambda do |row, rownum|
         if row[header[:name]].blank?
           next if current_product.nil?
           report_error.call rownum, add_fba_sku.call(row)
@@ -140,26 +148,71 @@ class FbaSpreadsheetUpload < ActiveRecord::Base
         end
       end
 
+      read_fba_job_template = lambda do |row, rownum|
+        fba_job_template = FbaJobTemplate.find_or_initialize_by name: row[header[:name]]
+        fba_job_template.job_name = row[header[:job_name]]
+        unless fba_job_template.save
+          report_error.call rownum, "Couldn't update job template info: #{fba_job_template.errors.full_messages.join(', ')}"
+          next
+        end
+
+        imprint_method_name = row[header[:imprint_method]]
+        if imprint_method_name.blank?
+          report_error.call rownum, "No imprint method supplied" and next
+        end
+        imprint_method = ImprintMethod.find_by name: imprint_method_name
+        if imprint_method.nil?
+          report_error.call rownum, "Couldn't find imprint method called \"#{imprint_method_name}\""
+          next
+        end
+
+        print_location_name = row[header[:print_location]]
+        if print_location_name.blank?
+          report_error.call rownum, "No print location supplied" and next
+        end
+        print_location = imprint_method.print_locations.find_by name: print_location_name
+        if print_location.blank?
+          report_error.call rownum, "Couldn't find print location for #{imprint_method_name}: \"#{print_location_name}\""
+          next
+        end
+
+        description = row[header[:description]]
+
+        imprint = fba_job_template.fba_imprint_templates.find_or_initialize_by(print_location_id: print_location.id)
+        imprint.description = description
+        unless imprint.save
+          report_error.call rownum, "Couldn't save imprint: #{imprint.errors.full_messages.join(', ')}"
+          next
+        end
+      end
+
       find_header = proc do |row|
         row.each_with_index do |cell, index|
           case cell.try(:downcase)
           when nil then next
-          when "name"                  then header[:name]       = index
-          when /(master|parent)\s+sku/ then header[:parent_sku] = index
-          when "msku"                  then header[:msku]       = index
-          when "fnsku"                 then header[:fnsku]      = index
-          when "asin"                  then header[:asin]       = index
-          when "brand"                 then header[:brand]      = index
-          when "style"                 then header[:style]      = index
-          when "color"                 then header[:color]      = index
-          when "size"                  then header[:size]       = index
-          when /job\s+template/        then header[:template]   = index
+          when "name"                  then header[:name]           = index
+          when /(master|parent)\s+sku/ then header[:parent_sku]     = index
+          when "msku"                  then header[:msku]           = index
+          when "fnsku"                 then header[:fnsku]          = index
+          when "asin"                  then header[:asin]           = index
+          when "brand"                 then header[:brand]          = index
+          when "style"                 then header[:style]          = index
+          when "color"                 then header[:color]          = index
+          when "size"                  then header[:size]           = index
+          when /job\s+template/        then header[:template]       = index
+          when /job\s+name/            then header[:job_name]       = index
+          when /imprint\s+method/      then header[:imprint_method] = index
+          when /print\s+location/      then header[:print_location] = index
+          when "description"           then header[:description]    = index
           end
         end
 
-        if %i(name parent_sku fnsku asin brand style color size template) - header.keys == [] &&
-           !header.values.any?(&:blank?)
-          action = read
+        unless header.values.any?(&:blank?)
+          if %i(name parent_sku fnsku asin brand style color size template) - header.keys == []
+            action = read_fba_product
+          elsif %i(name job_name imprint_method print_location description) - header.keys == []
+            action = read_fba_job_template
+          end
         end
       end
 
