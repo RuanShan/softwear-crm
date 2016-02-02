78111494e6e017639f2e4f51133f473442e8de9a
Nigel Baillie <metreckk@gmail.com>
2016-02-02 15:55:40 -0500

    improve order state header

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -15,7 +15,7 @@ class OrdersController < InheritedResources::Base
   def update
     super do |success, failure|
       success.html do
-        if @order.fba? && order.invoice_state_changed? && order.ready_for_production?
+        if ((@order.fba? && order.invoice_state_changed? && order.ready_for_production?) rescue false)
           flash[:success] = "The order has been signed off and will be sent to production shortly."
         end
         redirect_to edit_order_path(params[:id], anchor: 'details')
