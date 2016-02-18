0a5b4e35a779fde65c3a5cca99746f4fc7ee60c6
Nigel Baillie <metreckk@gmail.com>
2016-02-18 18:29:34 -0500

    don't drop users table

--- db/migrate/20160122223343_revise_users.rb
+++ db/migrate/20160122223343_revise_users.rb
@@ -30,49 +30,11 @@ class ReviseUsers < ActiveRecord::Migration
     # 0 is the special ID of the customer user in the new system.
     Payment.unscoped.where(salesperson_id: 52).update_all salesperson_id: 0
 
-    drop_table :users
+    rename_table :users, :old_users
   end
 
   def down
     drop_table :user_attributes
-    # Pasted in from the create_users migration
-    create_table(:users) do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-
-      ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-
-      ## Rememberable
-      t.datetime :remember_created_at
-
-      ## Trackable
-      t.integer  :sign_in_count, default: 0, null: false
-      t.datetime :current_sign_in_at
-      t.datetime :last_sign_in_at
-      t.string   :current_sign_in_ip
-      t.string   :last_sign_in_ip
-
-      ## Confirmable
-      t.string   :confirmation_token
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
-
-      ## Lockable
-      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      t.string   :unlock_token # Only if unlock strategy is :email or :both
-      t.datetime :locked_at
-
-
-      t.timestamps
-    end
-
-    add_index :users, :email,                unique: true
-    add_index :users, :reset_password_token, unique: true
-    add_index :users, :confirmation_token,   unique: true
-    add_index :users, :unlock_token,         unique: true
+    rename_table :old_users, :users
   end
 end
