cb1369c684d60d27bd17eb2d2e6b538ea5db651b
Nigel Baillie <metreckk@gmail.com>
2016-02-22 12:00:15 -0500

    finally fix specs (maybe) and update softwear-lib with better token authentication logging

--- spec/views/shared/_activity_list_item.html.erb_spec.rb
+++ spec/views/shared/_activity_list_item.html.erb_spec.rb
@@ -5,15 +5,16 @@ describe 'shared/_activity_list_item.html.erb' do
   let!(:activity2) { create(:quote_activity_line_item_update) }   
 
   before(:each) do 
-      allow(view).to receive(:render_activity).and_return("")
-      render partial: 'shared/activity_list_item', locals: {activity: activity1 }
+    allow(view).to receive(:render_activity).and_return("")
+    allow(view).to receive(:user_path).and_return "/users/"
+    render partial: 'shared/activity_list_item', locals: {activity: activity1 }
   end 
 
   it 'has the crm user who made the change' do 
-      expect(rendered).to have_css("a[href^='/users/']")  
+    expect(rendered).to have_css("a[href^='/users/']")  
   end
 
   it 'has a timestamp' do 
-      expect(rendered).to have_css(".time")
+    expect(rendered).to have_css(".time")
   end
 end
