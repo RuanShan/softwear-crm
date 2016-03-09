264d876c943d95cee3aab5092e23b49109593a63
Stefan Gale <stefan@wolverinetshirtcompany.com>
2016-03-09 13:54:16 -0500

    Added disclaimer to order/show

--- spec/views/orders/show.html.erb_spec.rb
+++ spec/views/orders/show.html.erb_spec.rb
@@ -3,27 +3,55 @@ require 'spec_helper'
 describe "orders/show", type: :view do
   
   let(:order) { build_stubbed(:order) }
+  let(:pickup_order) { build_stubbed(:order, delivery_method: "Pickup in Ann Arbor") }
 
-  before(:each) do 
-    assign(:order, order)
-    render
-  end
+  context "Given an order with a shipping delivery method" do
+    
+    before{
+      assign(:order, order)
+      render
+    }
 
-  it "Displays the store's name at the head of the invoice" do 
-    expect(rendered).to have_css :h1, text: "#{order.store.name}"  
-  end
-
-  it "Displays the store's address at the foot of the invoice" do  
-    within("#invoice_footer") do 
-      order.store.address_array.each do |addr_field|
-        expect(rendered).to have_text addr_field
-      end  
-      expect(rendered).to have_text order.store.phone 
-      expect(rendered).to have_text order.store.sales_email 
+    it 'should display the disclaimer' do
+      expect(rendered).to have_text "Disclaimer" 
     end
   end
 
-  context 'The store has a logo' do 
-    it "Displays the store logo at the header of the invoice"
+  context 'Given an order with a pick up delivery method' do
+    
+    before{
+      assign(:order, pickup_order)
+      render
+    }
+
+    it 'should not display the disclaimer' do
+      expect(rendered).to_not have_text "Disclaimer"
+    end
+  end
+
+  context 'Given a header and a footer on an invoice' do
+    
+    before(:each) do 
+      assign(:order, order)
+      render
+    end
+
+    it "Displays the store's name at the head of the invoice" do 
+      expect(rendered).to have_css :h1, text: "#{order.store.name}"  
+    end
+
+    it "Displays the store's address at the foot of the invoice" do  
+      within("#invoice_footer") do 
+        order.store.address_array.each do |addr_field|
+          expect(rendered).to have_text addr_field
+        end  
+        expect(rendered).to have_text order.store.phone 
+        expect(rendered).to have_text order.store.sales_email 
+      end
+    end
+
+    context 'The store has a logo' do 
+      it "Displays the store logo at the header of the invoice"
+    end
   end
 end
