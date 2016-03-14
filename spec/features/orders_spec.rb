92c2071acabc13a8da16bfe8e33c4834bfb22a44
Stefan Gale <stefan@wolverinetshirtcompany.com>
2016-03-14 17:24:29 -0400

    Fixed specs. Works now.

--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -182,11 +182,11 @@ feature 'Order management', order_spec: true, js: true do
     click_link "show_#{payment1.id}"
     wait_for_ajax
 
-    expect(page).to have_link "Return To Order Payments"
+    expect(page).to have_link "Return To Order"
     expect(page).to have_link "Print"
     expect(page).to have_text "Payment Receipt ##{payment1.id}"
 
-    click_link 'Return To Order Payments'
+    click_link 'Return To Order'
     wait_for_ajax
 
     click_link "show_#{payment2.id}"
