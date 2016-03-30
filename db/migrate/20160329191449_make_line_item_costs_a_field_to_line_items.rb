9206dea98ec866a2bd9b93b5658cc92456879c17
Nigel Baillie <metreckk@gmail.com>
2016-03-30 11:12:11 -0400

    woops, the migration accessed the association it removed

--- db/migrate/20160329191449_make_line_item_costs_a_field_to_line_items.rb
+++ db/migrate/20160329191449_make_line_item_costs_a_field_to_line_items.rb
@@ -1,9 +1,17 @@
+class LineItemWithCost < LineItem
+  has_one :cost, as: :costable
+end
+
 class MakeLineItemCostsAFieldToLineItems < ActiveRecord::Migration
   def up
-    add_column :line_items, :cost_amount, :decimal, scale: 2, precision: 10
+    begin
+      add_column :line_items, :cost_amount, :decimal, scale: 2, precision: 10
+    rescue Mysql2::Error => _e
+      puts "  cost_amount already added"
+    end
 
     unless Rails.env.test?
-      LineItem.unscoped.joins(:cost).find_each do |li|
+      LineItemWithCost.unscoped.joins(:cost).find_each do |li|
         if li.cost.try(:amount)
           li.update_column :cost_amount, li.cost.amount
         end
