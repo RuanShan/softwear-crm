73feecf8b90a93c551c6b7b3e308c5dd91b2c5a0
Nigel Baillie <metreckk@gmail.com>
2016-04-15 11:05:07 -0400

    fix home page hopefully

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,6 +9,7 @@ class HomeController < ApplicationController
       with(:state, :artwork_request_rejected)
       with(:salesperson_full_name,  current_user.full_name)
     end.results
+      .reject { |ar| ar.order.nil? }
 
     @pending_quote_requests = QuoteRequest.search do
       with(:status, 'assigned')
