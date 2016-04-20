5ea70533a2751cf2170bf40a4c4baf073179af1d
Nigel Baillie <metreckk@gmail.com>
2016-04-20 14:39:01 -0400

    someone left a byebug in here

--- db/schema.rb
+++ db/schema.rb
@@ -320,11 +320,11 @@ ActiveRecord::Schema.define(version: 20160418195245) do
 
   create_table "fba_spreadsheet_uploads", force: :cascade do |t|
     t.boolean  "done"
-    t.text     "spreadsheet",       limit: 65535
+    t.text     "spreadsheet",       limit: 4294967295
     t.text     "processing_errors", limit: 65535
     t.string   "filename",          limit: 191
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
   create_table "freshdesk_local_contacts", force: :cascade do |t|
@@ -529,6 +529,8 @@ ActiveRecord::Schema.define(version: 20160418195245) do
 
   add_index "jobs", ["deleted_at"], name: "index_jobs_on_deleted_at", using: :btree
   add_index "jobs", ["fba_job_template_id"], name: "index_jobs_on_fba_job_template_id", using: :btree
+  add_index "jobs", ["jobbable_id"], name: "index_jobs_jobbable_id", using: :btree
+  add_index "jobs", ["jobbable_type"], name: "index_jobs_jobbable_type", using: :btree
 
   create_table "line_item_groups", force: :cascade do |t|
     t.string   "name",        limit: 191
@@ -650,9 +652,9 @@ ActiveRecord::Schema.define(version: 20160418195245) do
     t.decimal  "shipping_price",                             precision: 10, scale: 2, default: 0.0
     t.string   "invoice_state",             limit: 191
     t.string   "production_state",          limit: 191
-    t.integer  "softwear_prod_id",          limit: 4
     t.string   "notification_state",        limit: 191
     t.integer  "freshdesk_proof_ticket_id", limit: 4
+    t.integer  "softwear_prod_id",          limit: 4
     t.string   "artwork_state",             limit: 191
     t.string   "customer_key",              limit: 191
     t.text     "invoice_reject_reason",     limit: 16777215
@@ -661,8 +663,8 @@ ActiveRecord::Schema.define(version: 20160418195245) do
     t.decimal  "discount_total",                             precision: 10, scale: 2
     t.decimal  "payment_total",                              precision: 10, scale: 2
     t.boolean  "imported_from_admin"
-    t.string   "phone_number_extension",    limit: 191
     t.string   "payment_state",             limit: 191
+    t.string   "phone_number_extension",    limit: 191
     t.boolean  "canceled"
     t.decimal  "tax_rate",                                   precision: 10, scale: 4
     t.decimal  "fee",                                        precision: 10, scale: 4
@@ -825,8 +827,8 @@ ActiveRecord::Schema.define(version: 20160418195245) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.decimal  "shipping",                                     precision: 10, scale: 2
-    t.datetime "initialized_at"
     t.string   "quote_source",                     limit: 191
+    t.datetime "initialized_at"
     t.string   "freshdesk_ticket_id",              limit: 191
     t.boolean  "informal"
     t.integer  "insightly_category_id",            limit: 4
