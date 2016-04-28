fd10782c300e15d1560411de11230750936fa0b8
Ricky Winowiecki <ricky@annarbortshirtcompany.com>
2016-04-28 16:43:43 -0400

    (1327) This will fix the phone numbers as it's used

--- app/models/crm/phone.rb
+++ app/models/crm/phone.rb
@@ -8,6 +8,8 @@ class Crm::Phone < ActiveRecord::Base
               message: 'is incorrectly formatted, use 000-000-0000'
             }
 
+  before_validation :reformat_number
+
   def full_number
     if extension.blank?
       number
@@ -15,4 +17,13 @@ class Crm::Phone < ActiveRecord::Base
       "#{number}x#{extension}"
     end
   end
+
+  private
+
+  def reformat_number
+    unless /\d{3}-\d{3}-\d{4}/.match(number)
+      number.insert(6, '-').insert(3, '-')
+    end
+  end
+
 end
