5708901a6185e89c6479f31aeee527e83b9f0ea4
Nigel Baillie <metreckk@gmail.com>
2016-05-03 14:53:40 -0400

    phone numbers now reformat the number on-validation instead of before

--- app/models/crm/phone.rb
+++ app/models/crm/phone.rb
@@ -2,11 +2,7 @@ class Crm::Phone < ActiveRecord::Base
   belongs_to :contact, class_name: 'Crm::Contact'
 
   validates :number, presence: true
-  validates :number,
-            format: {
-              with: /\d{3}-\d{3}-\d{4}/,
-              message: 'is incorrectly formatted, use 000-000-0000'
-            }
+  validate :phone_number_formatted_correctly
 
   before_validation :reformat_number
 
@@ -19,6 +15,19 @@ class Crm::Phone < ActiveRecord::Base
   end
 
   private
+
+  # message: 'is incorrectly formatted, use 000-000-0000'
+  def phone_number_formatted_correctly
+    return if number.blank? || number =~ /\d{3}-\d{3}-\d{4}/
+
+    d = number.each_char.select { |c| c =~ /\d/ }
+    if d.size < 10
+      errors.add(:number, "is incorrectly formatted, use 000-000-0000")
+      return
+    end
+
+    self.number = "#{d[0..2].join}-#{d[3..5].join}-#{d[6..9].join}"
+  end
 
   def reformat_number
     if number && /\d{3}\d{3}\d{4}/.match(number)
--- spec/models/crm/phone_spec.rb
+++ spec/models/crm/phone_spec.rb
@@ -8,7 +8,9 @@ describe Crm::Phone, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of :number }
     it { is_expected.to allow_value('123-654-9871').for :number }
-    it { is_expected.to_not allow_value('135184e6').for(:number)
+    it { is_expected.to allow_value('1236549871').for :number and expect(subject.number).to eq '123-654-9871' }
+    it { is_expected.to allow_value('(123) 654-9871').for :number and expect(subject.number).to eq '123-654-9871' }
+    it { is_expected.to_not allow_value('134e6').for(:number)
                               .with_message('is incorrectly formatted, use 000-000-0000') }
   end
 
